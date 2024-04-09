3caf814f7011d34d06e600573eaf27f443f7710d
Nicky <yuthapong@magicboxsolution.com>
2024-04-09 16:31:47 +0700

    commit

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+require("dotenv").config();
+
 const app = express();
 const PORT = process.env.PORT || 3005;
 
@@ -34,6 +36,6 @@ app.post("/echo", (req, res) => {
   res.json(req.body);
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen("3005", () => {
+  console.log(`Server running on port`);
 });
